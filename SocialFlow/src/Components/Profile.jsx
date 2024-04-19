25413449263c356e120e6b9bb6b9806891c0b5f2
Hyder Danish <hyder.danish369@gmail.com>
2024-04-19 16:55:07 +0530

    done

--- SocialFlow/src/Components/Profile.jsx
+++ SocialFlow/src/Components/Profile.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import UserId from "./storage/user-id";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import Explorepage from "./Explorepage";
 import "./styles/userprofile.css"
 const Profile=()=>{
@@ -12,24 +13,22 @@ const Profile=()=>{
     const fetchuserdata=async()=>{
         try{
          const responce= await axios.get(`http://localhost:5000/users/singleuser/${userid}`); 
-         Setprofiledata(responce.data.user); 
+         console.log(responce.data.user);
+         Setprofiledata(responce.data.user.profile); 
          console.log(profiledata,"he");
         }catch(err){
             console.log(err);
         }
     }
-    console.log(profiledata,"he");
     const bURL="http://localhost:5000/upload"
     return(
         <div className="Profile-mainpage w-full flex flex-wrap justify-center bg-red-400">
             <Explorepage/>
             <div className="profile-datail-L">
             <div className="userprofile" >
-            {profiledata.map((data)=>(
-                <>
-            <img src={`${bURL}/${data.profile[0]}`} alt="" />
-                </>
-            ))}
+            <img className='userPic'src={`${bURL}/${profiledata[0]}`} alt="img" />
+            
+                
             </div>
             </div>
         
