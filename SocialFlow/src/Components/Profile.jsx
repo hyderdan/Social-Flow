dafa93e432073c55c45d43a15ca6526dbbfb3dbb
Hyder Danish <hyder.danish369@gmail.com>
2024-04-21 20:04:23 +0530

    done

--- SocialFlow/src/Components/Profile.jsx
+++ SocialFlow/src/Components/Profile.jsx
@@ -6,6 +6,7 @@ import Explorepage from "./Explorepage";
 import "./styles/userprofile.css"
 const Profile=()=>{
     const[profiledata,Setprofiledata]=useState([]);
+    const[profiledata2,Setprofiledata2]=useState([]);  
     const userid=UserId();
     useEffect(()=>{
         fetchuserdata()
@@ -14,6 +15,7 @@ const Profile=()=>{
         try{
          const responce= await axios.get(`http://localhost:5000/users/singleuser/${userid}`); 
          console.log(responce.data.user);
+         Setprofiledata2(responce.data.user);
          Setprofiledata(responce.data.user.profile); 
          console.log(profiledata,"he");
         }catch(err){
@@ -25,11 +27,23 @@ const Profile=()=>{
         <div className="Profile-mainpage w-full flex flex-wrap justify-center bg-red-400">
             <Explorepage/>
             <div className="profile-datail-L">
-            <div className="userprofile" >
-            <img className='userPic'src={`${bURL}/${profiledata[0]}`} alt="img" />
-            
-                
+            <div className="userprofile-L" >
+            <img className='userprofilepic-L'src={`${bURL}/${profiledata[0]}`} alt="img" />
+            <h1>{profiledata2.username}</h1>
+            <ul>
+                <li><button>Edit profile</button></li>
+                <li><button>Archive</button></li>
+            </ul>
+            <div className="userprofile-L-list2">
+            <ul>
+                <li>Posts</li>
+                <li>Followers</li>
+                <li>Following</li>
+            </ul>
+            </div> 
+            <p>{profiledata2.bio}</p>  
             </div>
+            <div className="highlight-L">+</div>
             </div>
         
         </div>
